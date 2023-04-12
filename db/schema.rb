50fd572283542ba0d3c0d472232fbffc48a5cc63
iSaacSigei <kiplangatisack.is@gmail.com>
2023-04-12 17:23:32 +0300

    changes in sessions handlers

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,32 @@
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_165723) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_094245) do
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lands", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "admin_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_lands_on_admin_id"
+    t.index ["user_id"], name: "index_lands_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -29,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_165723) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
   end
 
+  add_foreign_key "lands", "admins"
+  add_foreign_key "lands", "users"
   add_foreign_key "profiles", "users"
 end
