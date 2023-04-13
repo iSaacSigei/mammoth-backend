9eb3b6d7f3c7f6dfcb92cbdc34e14eb8a17517de
Gibson-Bett <gibubett@gmail.com>
2023-04-13 11:53:41 +0300

    minor changes

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -1,17 +1,22 @@
 class LandsController < ApplicationController
-    def create
-        @land = Land.new(land_params)
-        @land.image = params[:image] # Assign the uploaded image to the image column
-        if @land.save
-          render json: { land: @land }
-        else
-          render json: { errors: @land.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-      
-      private
-      
-      def land_params
-        params.require(:land).permit(:name, :description)
-      end            
+# app/controllers/lands_controller.rb
+
+  def create
+    @land = Land.new(land_params)
+    # @land.user = User.find(params[:user_id])
+    # @land.admin = Admin.find(params[:admin_id])
+
+    if @land.save
+      render json: @land, status: :created
+    else
+      render json: { errors: @land.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def land_params
+    params.require(:land).permit(:name, :description, :image)
+  end
 end
+        
