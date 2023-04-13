2cece49ee788832182393f3abf5ea497569bdacb
Gibson-Bett <gibubett@gmail.com>
2023-04-13 10:38:16 +0300

    add create method to lands controller

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -1,2 +1,17 @@
 class LandsController < ApplicationController
+    def create
+        @land = Land.new(land_params)
+        @land.image = params[:image] # Assign the uploaded image to the image column
+        if @land.save
+          render json: { land: @land }
+        else
+          render json: { errors: @land.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+      
+      private
+      
+      def land_params
+        params.require(:land).permit(:name, :description)
+      end            
 end
