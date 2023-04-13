351671adb7938409b35a40eb943ff341bcc00c9a
Gibson-Bett <gibubett@gmail.com>
2023-04-13 13:00:13 +0300

    fixed bugs

--- db/migrate/20230413073501_add_name_to_lands.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToLands < ActiveRecord::Migration[7.0]
-  def change
-    add_column :lands, :name, :string
-  end
-end
--- db/migrate/20230413073620_add_description_to_lands.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToLands < ActiveRecord::Migration[7.0]
-  def change
-    add_column :lands, :description, :string
-  end
-end
