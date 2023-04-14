07943643d89094e8285b487234cd504a9d58e74f
iSaacSigei <kiplangatisack.is@gmail.com>
2023-04-14 16:17:22 +0300

    addded more columns to lands

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   end
 
   post '/login', to: 'sessions#create'
-  
+  get 'user', to: "users#show"
   post '/accept_quotation/:id', to: 'lands#accept_quotation', as: 'accept_quotation'
   patch '/admins/:admin_id/lands/:land_id', to: 'admins#update_price'
   
